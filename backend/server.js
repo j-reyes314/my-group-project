f2345fe6848db9dcf39e0438898d31a4a0e36f04
Chris Espinal <christopher.espinal@lc.cuny.edu>
2021-06-18 21:03:49 -0400

    Modified server for Campus model

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,13 @@ let Student = sequelize.define('Student',{
     gpa: Sequelize.FLOAT
 });
 
+let Campus = sequelize.define('Campus',{
+    campusName: Sequelize.STRING,
+    imageURL: Sequelize.STRING,
+    address: Sequelize.STRING,
+    description: Sequelize.STRING
+});
+
 
 // try{
 //     sequelize.authenticate().then(console.log("The connection has been established."))
@@ -59,6 +66,30 @@ app.get('/Students', async function(request, response) {
     response.json(test);
 })
 
+app.get('/Campus', async function(request, response) {
+    // dStuent.findAll().then(students => res.json(students))
+
+ 
+    let test2 =  await Campus.findAll();
+
+    console.log(test2);
+    //Student.findAll().then(function(rows) {
+
+     //   console.log(row);
+
+
+       // for(var i = 0; i < rows.length; i++) {
+       // var columnData = rows[i].dataValues;
+        //var name = columnData.firstname + " " + columnData.lastname;
+    //console.log(columnData );
+    //someData.append(columnData);
+        //}
+        //console.log(someData);  
+    //});
+    
+    response.json(test2);
+})
+
 app.post('/Students', function(request, response){
 
     console.log(request.body);
@@ -82,7 +113,7 @@ app.post('/Students', function(request, response){
     // });
 
     Student.sync().then(function(){
-        console.log("The table is ready to be used");
+        console.log("The student table is ready to be used");
     })
 
 
@@ -110,6 +141,60 @@ app.post('/Students', function(request, response){
     });
 
     response.send("The form has been received");
+    
+
+});
+
+app.post('/Campus', function(request, response){
+
+    console.log(request.body);
+
+    let campusName = request.body.campusName;
+    let imageURL = request.body.imageURL;
+    let address = request.body.address;
+    let description= request.body.description;
+
+
+    //console.log(request);
+    console.log("We are getting information from the front end");
+
+    // let Student = sequelize.define('Student',{
+    //     firstname: Sequelize.STRING,
+    //     lastname: Sequelize.STRING,
+    //     email: Sequelize.STRING,
+    //     school: Sequelize.STRING,
+    //     gpa: Sequelize.FLOAT
+    // });
+
+    Campus.sync().then(function(){
+        console.log("The campus table is ready to be used");
+    })
+
+
+    Campus.findOne({
+        where:{
+            address: address 
+        }
+    }).then(function(campus){
+        if(!campus){
+            console.log("false");
+
+            Campus.create({
+                campusname: campusName,
+                imageurl: imageURL,
+                email: address,
+                school: description,
+                gpa: gpa
+            });
+
+        }else{
+            console.log("campus");
+            // Notify the student that this user already exists in the table
+        }
+
+    });
+
+    response.send("The campus form has been received");
     
 
 });
